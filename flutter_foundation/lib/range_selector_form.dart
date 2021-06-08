1485bb2c7044c3d67d78f39a386e6888a87d5e18
kwonym3094 <kwonym3094@gmail.com>
2021-06-08 22:38:04 +0900

    [study] basic state management

---
+++ flutter_foundation/lib/range_selector_form.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+typedef IntValueSetter = void Function(int value);
+
+class RangeSelectorForm extends StatelessWidget {
+  const RangeSelectorForm({
+    Key? key,
+    required this.formKey,
+    required this.minValueSetter,
+    required this.maxValueSetter,
+  }) : super(key: key);
+
+  final GlobalKey<FormState> formKey;
+  final IntValueSetter minValueSetter;
+  final IntValueSetter maxValueSetter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RangeSelectorTextFormField(
+              labelText: "Minimum",
+              intValueSetter: minValueSetter,
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            RangeSelectorTextFormField(
+              labelText: "Maximum",
+              intValueSetter: maxValueSetter,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RangeSelectorTextFormField extends StatelessWidget {
+  RangeSelectorTextFormField({
+    Key? key,
+    required this.labelText,
+    required this.intValueSetter,
+  }) : super(key: key);
+
+  final String labelText;
+  final IntValueSetter intValueSetter;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: labelText,
+      ),
+      keyboardType: TextInputType.numberWithOptions(
+        decimal: false,
+        signed: true,
+      ),
+      onSaved: (newValue) => intValueSetter(int.parse(newValue ?? "")),
+      validator: (value) {
+        if (value == null || int.tryParse(value) == null) {
+          return "Must be an integer";
+        } else {
+          return null;
+        }
+      }, // return null => 모두 validated 됬다
+    );
+  }
+}
