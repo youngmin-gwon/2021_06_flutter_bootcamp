4fa62e40ab416f90825d5e97b64abc5c741e89b9
youngmin-gwon <youngmin.gwon@gmail.com>
2021-06-09 23:25:48 +0900

    [study] state management with 1. basic flutter plugin 2. provider 3. riverpod

--- flutter_foundation/lib/main.dart
+++ flutter_foundation/lib/main.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_foundation/randomizer_change_notifier.dart';
 import 'package:flutter_foundation/range_selector_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(AppWidget());
 }
 
+// 이렇게 정의함으로서 어느 곳이나 들어갈 수 있게 됨
+// global variable 처럼 보이나 클래스와 같은 레벨에 있기 때문에 global variable 이 아님 => mutable 하지 않고 immutable 하다는 의미
+// 한번 instantiated 되면 앱이 꺼질 때, 혹은 widget dispose 전까지 남아있음
+final randomizerProvider =
+    ChangeNotifierProvider((ref) => RandomizerChangeNotifier());
+
 class AppWidget extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Randomizer',
-      home: RangeSelector(),
+    // provider 를 사용하면서 생기는 문제 : provider widget을 여러개 제공하고 싶을 때 코드가 복잡해보이기 쉬움
+    // 정의된 type 에만 기대고 있는데 만약 2개의 같은 type의 changenotifier를 사용할 때 가장 가까운 것을 참고해서 문제가 생김
+    return ProviderScope(
+      // riverpod의 provider는 내부에 정의되어 있지 않다. 그럼 어디에 정의되어있는가? => anywhere!
+      // return ChangeNotifierProvider(
+      // create: (context) => RandomizerChangeNotifier(),
+      // 이렇게 정의함으로서 RandomizerChangeNotifier을 앱 모든 곳에서 접근할 수 있게 됨
+      child: MaterialApp(
+        title: 'Randomizer',
+        home: RangeSelector(),
+      ),
     );
   }
 }
--- flutter_foundation/lib/range_selector_page.dart
+++ flutter_foundation/lib/range_selector_page.dart
@@ -2,28 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_foundation/randomizer.dart';
 import 'package:flutter_foundation/range_selector_form.dart';
 
-class RangeSelector extends StatefulWidget {
+// 만약 state를 뽑아내고 아예 상관 없는 widget tree로 뽑아내고 싶을 때? => mutable state management => ChangeNotifier
+// mutable state management? (hooks는 kinda immutable state management)
+// => setState
+// 의미 : 해당 영역에서 field를 재할당한다
+class RangeSelector extends StatelessWidget {
   RangeSelector({Key? key}) : super(key: key);
 
-  @override
-  _RangeSelectorState createState() => _RangeSelectorState();
-}
-
-class _RangeSelectorState extends State<RangeSelector> {
   final formKey = GlobalKey<FormState>();
-  int _min = 0;
-  int _max = 0;
 
   @override
   Widget build(BuildContext context) {
+    // hooks 기능 : build 안에서만 가능
+    // hooks 특이한점? => 순서를 타기 때문에 순서를 바꿔줬다면 hot restart 해주기
+    // final min = useState<int>(0);
+    // final max = useState<int>(0);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Select Range"),
       ),
       body: RangeSelectorForm(
         formKey: formKey,
-        minValueSetter: (value) => _min = value,
-        maxValueSetter: (value) => _max = value,
+        // minValueSetter: (value) => min.value = value,
+        // maxValueSetter: (value) => max.value = value,
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_forward),
@@ -32,8 +34,8 @@ class _RangeSelectorState extends State<RangeSelector> {
             formKey.currentState?.save();
             Navigator.of(context).push(MaterialPageRoute(
                 builder: (context) => RandomizerPage(
-                      min: _min,
-                      max: _max,
+                    // min: min.value,
+                    // max: max.value,
                     )));
           }
         },
