ced63fbf59ab27aca736fa6572ab5843e0086a31
youngmin-gwon <youngmin.gwon@gmail.com>
2021-06-17 23:08:10 +0900

    [study] github authentication login in infrastructure layer

---
+++ repo_viewer/lib/auth/application/auth_notifier.dart
@@ -0,0 +1,30 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repo_viewer/auth/domain/auth_failure.dart';
+import 'package:repo_viewer/auth/infrastructure/github_authenticator.dart';
+
+part 'auth_notifier.freezed.dart';
+
+@freezed
+class AuthState with _$AuthState {
+  const AuthState._();
+  const factory AuthState.initial() = _Initial;
+  const factory AuthState.unauthenticated() = _Unauthenticated;
+  const factory AuthState.authenticated() = _Authenticated;
+  const factory AuthState.failure(AuthFailure failure) = _Failure;
+}
+
+typedef AuthUriCallback = Future<Uri> Function(Uri authorizationUrl);
+
+class AuthNotifier extends StateNotifier<AuthState> {
+  final GithubAuthenticator _authenticator;
+  AuthNotifier(this._authenticator) : super(const AuthState.initial());
+
+  Future<void> checkAndUpdateAuthStatus() async {
+    state = (await _authenticator.isSignedIn())
+        ? const AuthState.authenticated()
+        : const AuthState.unauthenticated();
+  }
+
+  Future<void> signIn(AuthUriCallback authorizationCallback) async {}
+}
