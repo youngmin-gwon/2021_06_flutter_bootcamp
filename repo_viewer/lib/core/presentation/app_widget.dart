2cd1a2681e46d93effebaba58ae95fd63e105e63
youngmin-gwon <youngmin.gwon@gmail.com>
2021-06-21 10:16:41 +0900

    [study] github login ui

---
+++ repo_viewer/lib/core/presentation/app_widget.dart
@@ -0,0 +1,51 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repo_viewer/auth/application/auth_notifier.dart';
+import 'package:repo_viewer/auth/shared/providers.dart';
+import 'package:repo_viewer/core/presentation/routes/app_router.gr.dart';
+
+final initializationProvider = FutureProvider<Unit>((ref) async {
+  final authNotifier = ref.read(authNotifierProvider.notifier);
+  await authNotifier.checkAndUpdateAuthStatus();
+  return unit;
+});
+
+class AppWidget extends ConsumerWidget {
+  final appRouter = AppRouter();
+
+  @override
+  Widget build(BuildContext context, WidgetReference ref) {
+    return ProviderListener(
+      provider: initializationProvider,
+      onChange: (context, value) {},
+      child: ProviderListener<AuthState>(
+        provider: authNotifierProvider,
+        onChange: (context, state) {
+          state.maybeMap(
+            authenticated: (_) {
+              appRouter.pushAndPopUntil(
+                const StarredReposRoute(),
+                // predicate
+                predicate: (route) => false,
+              );
+            },
+            unauthenticated: (_) {
+              appRouter.pushAndPopUntil(
+                const SignInRoute(),
+                predicate: (route) => false,
+              );
+            },
+            orElse: () {},
+          );
+        },
+        child: MaterialApp.router(
+          title: 'Repo Viewer',
+          routerDelegate: appRouter.delegate(),
+          routeInformationParser: appRouter.defaultRouteParser(),
+        ),
+      ),
+    );
+  }
+}
