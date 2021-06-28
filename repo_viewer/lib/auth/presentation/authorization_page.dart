9a248f5ec2f6cdb08ac75d09769e4b50d33ccbb2
youngmin-gwon <youngmin.gwon@gmail.com>
2021-06-28 17:13:22 +0900

    [study] sign-in webview

---
+++ repo_viewer/lib/auth/presentation/authorization_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:repo_viewer/auth/infrastructure/github_authenticator.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class AuthorizationPage extends StatefulWidget {
+  const AuthorizationPage({
+    Key? key,
+    required this.authorizationUrl,
+    required this.onAuthorizationCodeRedirectAttempt,
+  }) : super(key: key);
+
+  final Uri authorizationUrl;
+  final void Function(Uri redirectUrl) onAuthorizationCodeRedirectAttempt;
+
+  @override
+  _AuthorizationPageState createState() => _AuthorizationPageState();
+}
+
+class _AuthorizationPageState extends State<AuthorizationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebView(
+          javascriptMode: JavascriptMode.unrestricted,
+          initialUrl: widget.authorizationUrl.toString(),
+          onWebViewCreated: (controller) {
+            controller.clearCache();
+            CookieManager().clearCookies();
+          },
+          navigationDelegate: (navReq) {
+            if (navReq.url
+                .startsWith(GithubAuthenticator.redirectUrl.toString())) {
+              widget.onAuthorizationCodeRedirectAttempt(Uri.parse(navReq.url));
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      ),
+    );
+  }
+}
--- repo_viewer/lib/core/presentation/routes/app_router.dart
+++ repo_viewer/lib/core/presentation/routes/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/annotations.dart';
+import 'package:repo_viewer/auth/presentation/authorization_page.dart';
 import 'package:repo_viewer/auth/presentation/sign_in_page.dart';
 import 'package:repo_viewer/splash/presentation/splash_page.dart';
 import 'package:repo_viewer/starred_repos/presentation/starred_repos_page.dart';
@@ -6,6 +7,7 @@ import 'package:repo_viewer/starred_repos/presentation/starred_repos_page.dart';
 @MaterialAutoRouter(routes: [
   MaterialRoute(page: SplashPage, initial: true),
   MaterialRoute(page: SignInPage, path: "/sign-in"),
+  MaterialRoute(page: AuthorizationPage, path: "/auth"),
   MaterialRoute(page: StarredReposPage, path: "/starred"),
 ], replaceInRouteName: "Page,Route")
 class $AppRouter {}
