92f7c439f474d04d9a20ec6578523a7ed1598f2a
youngmin-gwon <youngmin.gwon@gmail.com>
2021-07-01 23:50:06 +0900

    [study] making dto for github repos

---
+++ prod_practice/android/app/src/main/kotlin/com/deukman/prod_practice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.deukman.prod_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- repo_viewer/lib/core/presentation/routes/app_router.dart
+++ repo_viewer/lib/core/presentation/routes/app_router.dart
@@ -1,8 +1,8 @@
 import 'package:auto_route/annotations.dart';
 import 'package:repo_viewer/auth/presentation/authorization_page.dart';
 import 'package:repo_viewer/auth/presentation/sign_in_page.dart';
+import 'package:repo_viewer/gitub/repos/starred_repos/presentation/starred_repos_page.dart';
 import 'package:repo_viewer/splash/presentation/splash_page.dart';
-import 'package:repo_viewer/starred_repos/presentation/starred_repos_page.dart';
 
 @MaterialAutoRouter(routes: [
   MaterialRoute(page: SplashPage, initial: true),
