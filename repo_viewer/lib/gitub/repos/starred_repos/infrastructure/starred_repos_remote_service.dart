9085ef0d7c19b57c6b8f6ab96798bf997b41df1b
dev_deukman <kwonym3094@gmail.com>
2021-07-04 23:50:24 +0900

    make github repo dtos

---
+++ repo_viewer/lib/gitub/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
@@ -0,0 +1,16 @@
+// import 'package:dio/dio.dart';
+
+// import 'package:repo_viewer/gitub/core/infrastructure/github_repo_dto.dart';
+
+// class StarredReposRemoteService {
+//   final Dio _dio;
+//   StarredReposRemoteService(
+//     this._dio,
+//   );
+
+//   Future<List<GithubRepoDTO>> getStarredReposPage(
+//     int page,
+
+//   ) async {}
+  
+// }
