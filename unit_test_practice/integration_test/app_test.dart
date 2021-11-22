32a68fa756df14699bd1891a53dd768d47b683c3
youngmin-gwon <youngmin.gwon@gmail.com>
2021-11-22 22:52:21 +0900

    widget and integration test

---
+++ unit_test_practice/integration_test/app_test.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:provider/provider.dart';
+import 'package:unit_test_practice/article.dart';
+import 'package:unit_test_practice/article_page.dart';
+import 'package:unit_test_practice/news_change_notifier.dart';
+import 'package:unit_test_practice/news_page.dart';
+import 'package:unit_test_practice/news_service.dart';
+
+class MockNewsService extends Mock implements NewsService {}
+
+void main() {
+  late MockNewsService mockNewsService;
+
+  setUp(
+    () {
+      mockNewsService = MockNewsService();
+    },
+  );
+
+  final articlesFromService = [
+    Article(title: "Test 1", content: "Test 1 content"),
+    Article(title: "Test 2", content: "Test 2 content"),
+    Article(title: "Test 3", content: "Test 3 content"),
+  ];
+
+  void arrangeNewsServiceReturns3Articles() {
+    when(() => mockNewsService.getArticles()).thenAnswer(
+      (_) async => articlesFromService,
+    );
+  }
+
+  Widget createWidgetUnderTest() {
+    return MaterialApp(
+      title: 'News App',
+      home: ChangeNotifierProvider(
+        create: (_) => NewsChangeNotifier(mockNewsService),
+        child: const NewsPage(),
+      ),
+    );
+  }
+
+  testWidgets(
+    """
+    Tapping on the first article excerpt opens the article page
+    where the full article content is displayed
+    """,
+    (WidgetTester tester) async {
+      arrangeNewsServiceReturns3Articles();
+      await tester.pumpWidget(createWidgetUnderTest());
+      await tester.pump();
+
+      await tester.tap(find.text("Test 1 content"));
+
+      await tester.pumpAndSettle();
+
+      expect(find.byType(NewsPage), findsNothing);
+      expect(find.byType(ArticlePage), findsOneWidget);
+
+      expect(find.text("Test 1"), findsOneWidget);
+      expect(find.text("Test 1 content"), findsOneWidget);
+    },
+  );
+}
